ee68819dc2dee41bc1198771257f979ca43b56e1
Taslim Oseni <tasleemoseni@gmail.com>
2022-04-19 01:53:45 +0100

    Add tests

--- tests/integrations/transfers.test.js
+++ tests/integrations/transfers.test.js
@@ -4,6 +4,9 @@ const nock = require('nock')
 const getTransferResponse = require('../responses/get_transfers_response_200.json')
 const getDepositsResponse = require('../responses/get_deposits_response_200.json')
 const getWithdrawalResponse = require('../responses/get_withdrawal_response_200.json')
+const getSingleTransferResponse = require('../responses/get_single_transfer_200.json')
+const getSingleDepositResponse = require('../responses/get_single_deposit_200.json')
+const getSingleWithdrawalResponse = require('../responses/get_single_withdrawal_200.json')
 
 
 
@@ -23,6 +26,14 @@ describe('Transfer functions work properly', function () {
       expect(await api.transfers.getTransfers()).to.eql(getTransferResponse)
     })
 
+    it('test_can_get_single_transfer', async function() {
+      nock(url)
+        .get('/transfers/7ce20bbdbb8748909185833cddc18e3d')
+        .reply(200, getSingleTransferResponse);
+
+      expect(await api.transfers.getSingleTransfer('7ce20bbdbb8748909185833cddc18e3d')).to.eql(getSingleTransferResponse)
+    })
+
 
     it('test_can_get_deposits', async function() {
       nock(url)
@@ -30,6 +41,15 @@ describe('Transfer functions work properly', function () {
         .reply(200, getDepositsResponse);
 
       expect(await api.transfers.getDeposits()).to.eql(getDepositsResponse)
+    })
+
+
+    it('test_can_get_single_deposit', async function() {
+      nock(url)
+        .get('/deposits/30420e27788f465caff439822bbe121c')
+        .reply(200, getSingleDepositResponse);
+
+      expect(await api.transfers.getSingleDeposit('30420e27788f465caff439822bbe121c')).to.eql(getSingleDepositResponse)
     })
 
 
@@ -41,6 +61,14 @@ describe('Transfer functions work properly', function () {
       expect(await api.transfers.getWithdrawals()).to.eql(getWithdrawalResponse)
     })
 
+
+    it('test_can_get_single_withdrawal', async function() {
+      nock(url)
+        .get('/withdrawals/a8e99f617b9a43a2b97ae27b143acd26')
+        .reply(200, getSingleWithdrawalResponse);
+
+      expect(await api.transfers.getSingleWithdrawal('a8e99f617b9a43a2b97ae27b143acd26')).to.eql(getSingleWithdrawalResponse)
+    })
 
 
   })
