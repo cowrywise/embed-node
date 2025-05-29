2a8882118f5c6351266370d6bc5d9f7eda4cd56c
taslimoseni <taslim@equalitie.org>
2025-05-29 21:06:02 +0100

    Update base_url key

--- src/helper/request.js
+++ src/helper/request.js
@@ -9,7 +9,7 @@ const perform = async function(config = Object, options = Object) {
   let _retry = true
 
   // URL
-  let url = process.env.BASE_URL;
+  let url = process.env.EMBED_API_BASE_URL;
   if(options['endpoint'].includes("/o/token")) { url += options['endpoint'] } else { url += "/api/v1" + options['endpoint'] }
 
 
