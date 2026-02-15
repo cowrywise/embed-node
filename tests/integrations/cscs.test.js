b5871c60e19a055f4c571751b519a6fe62cb62e3
Taslim Oseni <tasleemoseni@gmail.com>
2026-02-15 16:27:41 +0100

    Update tests to reflect API changes

---
+++ tests/integrations/cscs.test.js
@@ -0,0 +1,34 @@
+const expect = require('expect.js')
+const nock = require('nock')
+
+const cscsResponse = require('../responses/cscs_response_200.json')
+
+const Client = require('../../src/client')
+const api = new Client({client_id: '****', client_secret: '****', embed_api_base_url: 'https://sandbox.embed.cowrywise.com/api/v1'});
+const url = 'https://sandbox.embed.cowrywise.com/api/v1'
+
+
+describe('CSCS functions work properly', function () {
+
+    it('test_can_onboard_cscs', async function() {
+      const onboardData = {
+        account_id: "acc_123",
+        cscs_number: "1234567890"
+      };
+      nock(url)
+        .post('/integration/cscs/onboarding', onboardData)
+        .reply(200, cscsResponse);
+
+      expect(await api.cscs.onboarding(onboardData)).to.eql(cscsResponse)
+    })
+
+
+    it('test_can_get_cscs_profile', async function() {
+      nock(url)
+        .get('/integration/cscs/onboarding?account_id=acc_123')
+        .reply(200, cscsResponse);
+
+      expect(await api.cscs.getProfile("acc_123")).to.eql(cscsResponse)
+    })
+
+})
